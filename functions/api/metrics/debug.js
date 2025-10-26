6a4d890dd858622ca7721c13a437d42282d92ca0
CogMyra <mangotti@cogmyra.com>
2025-10-26 13:20:38 -0700

    Update debug.js

--- functions/api/metrics/debug.js
+++ functions/api/metrics/debug.js
@@ -1,10 +1,22 @@
 export async function onRequestGet({ env }) {
-  return new Response(
-    JSON.stringify({
+  try {
+    const summary = {
+      ok: true,
       hasEnv: !!env,
       hasCmgDb: !!env?.cmg_db,
-      typeOfCmgDb: typeof env?.cmg_db
-    }, null, 2),
-    { headers: { "content-type": "application/json" } }
-  );
+      typeOfCmgDb: typeof env?.cmg_db,
+      // show a few binding names so we know we're reading env
+      envKeysSample: env ? Object.keys(env).sort().slice(0, 10) : []
+    };
+
+    return new Response(JSON.stringify(summary, null, 2), {
+      headers: { "content-type": "application/json" }
+    });
+  } catch (err) {
+    // If anything goes wrong, we still return visible text
+    return new Response(`debug error:\n${err?.stack || err}`, {
+      status: 500,
+      headers: { "content-type": "text/plain" }
+    });
+  }
 }
